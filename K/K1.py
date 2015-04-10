04e0a40c0a88609d087857a54dba632827d67101
Alex <ao@wheely.com>
2015-04-10 11:34:41 +0300

    xgb

--- K/K1.py
+++ K/K1.py
@@ -1,1 +1,8 @@
 __author__ = 'alex'
+import pandas as pd
+import xgboost as xgb
+
+data = pd.DataFrame.from_csv('train.csv')
+y = data.target.apply(lambda x: x[-1])
+data.drop('target', axis=1, inplace=True)
+X = data.values
